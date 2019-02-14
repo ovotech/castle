f22eda176c958c57b0bc1822c5e8b8d91fdac1d4
Ivan Kerin <ikerin@gmail.com>
2019-02-14 13:40:03 +0200

    Add avro ts cli

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,4 @@
-import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import { readdirSync, readFileSync, unlinkSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { avroTs } from '../src';
 import { RecordType } from '../src/types';
@@ -6,6 +6,12 @@ import { RecordType } from '../src/types';
 const avscFiles = readdirSync(join(__dirname, 'avro'));
 
 describe('Avro ts test', () => {
+  beforeAll(() => {
+    readdirSync(join(__dirname, '__generated__'))
+      .filter(file => file.endsWith('.ts'))
+      .forEach(file => unlinkSync(join(__dirname, '__generated__', file)));
+  });
+
   it.each(avscFiles)('Should convert %s successfully', file => {
     const avro: RecordType = JSON.parse(String(readFileSync(join(__dirname, 'avro', file))));
     const ts = avroTs(avro, { 'timestamp-millis': 'string', date: 'string' });
