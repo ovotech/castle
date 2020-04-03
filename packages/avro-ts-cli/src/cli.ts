b3631e77a67fb0f82fb3df53013a1f2bc70b0839
ivank <ikerin@gmail.com>
2020-04-03 10:25:57 +0300

    Use more reliable cli execution method

--- packages/avro-ts-cli/src/cli.ts
+++ packages/avro-ts-cli/src/cli.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 import { convert } from './convert';
 
 convert().parse(process.argv);
--- packages/castle-cli/src/cli.ts
+++ packages/castle-cli/src/cli.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 import { castle } from './commands/castle';
 
 castle().parse(process.argv);
