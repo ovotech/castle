1d98485e4f00a99d22e0ef0c49414668e0c7c486
Ivan Kerin <ikerin@gmail.com>
2022-09-05 17:00:46 +0300

    Fix

--- packages/avro-ts/src/types/enum.ts
+++ packages/avro-ts/src/types/enum.ts
@@ -9,20 +9,20 @@ export const isEnumType = (type: Schema): type is schema.EnumType =>
 
 export const convertEnumType: Convert<schema.EnumType> = (context, schema) => {
   const namespace = schema.namespace ?? context.namespace;
-  const type = context.withTypescriptEnums === true ?
-    Node.Enum({
-      name: convertName(firstUpperCase(schema.name)),
-      members: schema.symbols.map(val => Node.EnumMember({ name: val, value: val })),
-      isExport: true,
-      jsDoc: schema.default ? withDefault(schema.default, schema.doc) : schema.doc,
-    })
-    :
-    Type.Alias({
-      name: convertName(firstUpperCase(schema.name)),
-      type: Type.Union(schema.symbols.map((symbol) => Type.Literal(symbol))),
-      isExport: true,
-      jsDoc: schema.doc,
-    });
+  const type =
+    context.withTypescriptEnums === true
+      ? Node.Enum({
+          name: convertName(firstUpperCase(schema.name)),
+          members: schema.symbols.map((val) => Node.EnumMember({ name: val, value: val })),
+          isExport: true,
+          jsDoc: schema.default ? withDefault(schema.default, schema.doc) : schema.doc,
+        })
+      : Type.Alias({
+          name: convertName(firstUpperCase(schema.name)),
+          type: Type.Union(schema.symbols.map((symbol) => Type.Literal(symbol))),
+          isExport: true,
+          jsDoc: schema.doc,
+        });
 
   return namedType(type, context, schema, namespace);
 };
